99368502fb717f9b25c83799331fc028811d8e68
HarshitPradhan <59720787+HarshitPradhan@users.noreply.github.com>
2021-03-31 23:47:07 +0530

    Kitchen Story

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { BackgroundCoverComponent } from './background-cover/background-cover.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    AuthenticationComponent,
+    BackgroundCoverComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
